0dcec733b03b477c025f76d80d9b0d146ee9c481
dren <drenkastrati33@gmail.com>
2024-05-29 13:39:29 +0200

    Adding the functionalilty to the catch the bird game

--- javascript/bird.js
+++ javascript/bird.js
@@ -2,3 +2,17 @@ function playAudio(audioId) {
     const audio = document.getElementById(audioId);
     audio.play();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const crowAnimation = document.querySelector('.crow-animation');
+    const crowImg = document.querySelector('.crow-img');
+    const content = document.querySelector('.content');
+    const cardTitle = document.querySelector('.crow');
+
+    crowAnimation.addEventListener('click', () => {
+        crowAnimation.classList.remove('active');
+        crowImg.classList.add('active');
+        content.classList.add('active');
+        cardTitle.textContent = "Crow";
+    });
+});
